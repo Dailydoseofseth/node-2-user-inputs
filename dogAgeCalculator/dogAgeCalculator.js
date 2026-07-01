75a3172bed77d68d8528eee0763c097903796cac
dailydoseofseth <dailydoseofseth@gmail.com>
2026-07-01 10:34:50 -0600

    ADD code comments

--- dogAgeCalculator/dogAgeCalculator.js
+++ dogAgeCalculator/dogAgeCalculator.js
@@ -9,24 +9,36 @@ node dogAgeCalculator.js
 
 */
 // process.argv is an array of everything typed in the terminal
-
+// Must have index [2] & [3] to get the dog name and human age, as [0] is the node command and [1] is the file name
 const dogName = process.argv[2];
 const humanAge = Number(process.argv[3]);
 
+// THIS VAR will hold  dog years VALUE, which we will calculate based on the human age
 let dogYears = "";
 
-// Calculate dog years/dogYears based on human age
-if (humanAge === 1) {
-  dogYears = 15;
-} else if (humanAge === 2) {
-  dogYears = 24;
+// Check if dogName and humanAge are provided, if not, log an error message
+// THIS IS NESTED IF/ELSE STATEMENTS WHICH IS A GOOD EXAMPLE OF HOW TO USE CONDITIONAL STATEMENTS IN JS
+if (!dogName || !humanAge) {
+  console.log("❌ Usage: node index.js - ENTER <dogName> <humanAge>");
 } else {
-  dogYears = 24 + (humanAge - 2) * 5;
+  // Calculate dogYears based on humanAge using IF/ELSE statements
+  if (humanAge === 1) {
+    dogYears = 15;
+  } else if (humanAge === 2) {
+    dogYears = 24;
+  } else {
+    dogYears = 24 + (humanAge - 2) * 5;
+  }
+
+  // log the result to the console using template literals
+  console.log(
+    `Your dog, ${dogName}, is ${humanAge} yrs old, AND that's ${dogYears} yrs old in dog years!`,
+  );
 }
 
-console.log(
-  `Your dog, ${dogName}, is ${humanAge} yrs old, AND that's ${dogYears} yrs old in dog years!`,
-);
+// ==============================================================================
+// I FIND THIS IMPORTANT SO I AM LEAVING IT HERE FOR FUTURE REFERENCE, AS IT IS A GOOD EXAMPLE OF USING TERNARY OPERATORS INSTEAD OF IF/ELSE STATEMENTS
+// ==============================================================================
 
 // ---------- advanced version of dogAgeCalculator.js ----------
 // ----------- using a ternary operator to calculate dog years -----------
